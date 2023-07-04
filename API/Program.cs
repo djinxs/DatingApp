f06cb692061ef180f0c649879d79e64ed58ee491
Jacco van Ingen <jaccovaningen@bmovelp.com>
2023-07-04 10:18:58 +0200

    AddAngular

--- API/Program.cs
+++ API/Program.cs
@@ -3,13 +3,19 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
+builder.Services.AddCors();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
 app.MapControllers();
+
 app.Run();
