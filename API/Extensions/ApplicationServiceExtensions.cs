3b17f288b3854dbedf875e12942fccec1c24b897
Jacco van Ingen <jaccovaningen@welkombijlevel.nl>
2023-07-19 10:59:29 +0200

    AddRepository

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -16,6 +16,7 @@ namespace API.Extensions
                 options.UseSqlite(config.GetConnectionString("DefaultConnection"));
             });
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
             return services;
         }
     }
